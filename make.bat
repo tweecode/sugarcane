036c3452dd447b865d5612e4f9b709ad7320c002
Chris Klimas <klimas@gmail.com>
2009-07-10 02:56:23 +0000

    fixed make script

--- make.bat
+++ make.bat
@@ -1,6 +1,7 @@
+del %~dp0compressed.js 
+del %~dp0temp*.js
 copy /B %~dp0*.js %~dp0temp.js
-
 findstr /V "console.log" %~dp0temp.js > temp2.js
 java -jar "c:\program files\yui\compressor.jar" -o %~dp0compressed.js %~dp0temp2.js
 java -jar "c:\program files\yui\compressor.jar" -o %~dp0compressed.css %~dp0style.css
-:: del %~dp0temp*.js 
+del %~dp0temp*.js 
